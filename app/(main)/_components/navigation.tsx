036e9b8e39d08923710319723d2dc62a9da24f2f
Joncarlos Perdomo <joncarlos.perdomo@whatifmediagroup.com>
2024-01-28 19:50:34 -0500

    Add documents page and navigation component to main layout

---
+++ app/(main)/_components/navigation.tsx
@@ -0,0 +1,11 @@
+const Navigation = () => {
+  return (
+    <>
+      <aside className="group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]">
+        Navigation
+      </aside>
+    </>
+  );
+};
+
+export default Navigation;
